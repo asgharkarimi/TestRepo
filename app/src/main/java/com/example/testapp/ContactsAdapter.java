c9a5a22609d64063d42a214d89e2f08ed56da1c2
asgharkarimi <AsgharGhozloo1>
2021-07-23 22:22:48 +0430

    show contactList

---
+++ app/src/main/java/com/example/testapp/ContactsAdapter.java
@@ -0,0 +1,66 @@
+package com.example.testapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsHolder> {
+    private ArrayList<String> contacts = new ArrayList<>();
+
+    public ContactsAdapter() {
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+        contacts.add("Asghar Karimi");
+    }
+
+    @NonNull
+    @Override
+    public ContactsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+        return new ContactsHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsHolder holder, int position) {
+        holder.bindContact(contacts.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public class ContactsHolder extends RecyclerView.ViewHolder {
+        private TextView tv_firstChar;
+        private TextView tv_fullName;
+
+        public ContactsHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_firstChar = itemView.findViewById(R.id.textViewfirstChar);
+            tv_fullName = itemView.findViewById(R.id.textViewFullname);
+        }
+
+        public void bindContact(String contactName) {
+            tv_firstChar.setText(contactName.substring(0, 1).toUpperCase());
+            tv_fullName.setText(contactName);
+        }
+    }
+}
--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewContacts;
+    private ContactsAdapter contactsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // comment added
+        recyclerViewContacts = findViewById(R.id.tv_contacts);
+        contactsAdapter = new ContactsAdapter();
+
+
+        recyclerViewContacts.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        recyclerViewContacts.setAdapter(contactsAdapter);
     }
 }
